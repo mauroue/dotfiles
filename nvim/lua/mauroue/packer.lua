a776e8c327f58c31e674f8780ec605ec49e94ea0
Mauro Tatsuya Ue <mauroue@gmail.com>
2023-09-07 20:48:10 -0300

    added snippets

--- nvim/lua/mauroue/packer.lua
+++ nvim/lua/mauroue/packer.lua
@@ -73,5 +73,14 @@ return require('packer').startup(function(use)
     }
   }
   use { "folke/neodev.nvim" }
+  use({
+      "L3MON4D3/LuaSnip",
+      -- follow latest release.
+      tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+      -- install jsregexp (optional!:).
+      run = "make install_jsregexp",
+      event = "BufReadPre",
+  })
+  use { "honza/vim-snippets" }
 
 end)
